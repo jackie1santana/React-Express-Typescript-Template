8101ab0ad612b77c678b5dc95ba7dc599c0c1a11
Jackie Santana <jackiebrown.nyc@gmail.comgit config --global user.email jackiebrown.nyc@gmail.com>
2020-04-11 17:59:52 -0500

    added type nodes

--- server.ts
+++ server.ts
@@ -1,13 +1,14 @@
-import express = require('express')
+import express, { Application, Request, Response, NextFunction } from 'express'
 
-const app = express()
+const app: Application = express()
 
-const PORT = process.env.PORT || 2300
+const PORT: any = process.env.PORT || 2300
 
-app.get('/', async (req, res) => {
+app.get('/', async (req: Request, res: Response, next: NextFunction) => {
     await res.send('Welcome to Express yo  sds')
 })
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`)
 })
+

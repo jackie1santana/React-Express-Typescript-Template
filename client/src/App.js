d65da1ed90de339cb5f2eba1f4b9948ed165a8c5
Jackie Santana <jackiebrown.nyc@gmail.comgit config --global user.email jackiebrown.nyc@gmail.com>
2020-04-18 16:25:26 -0500

    testing particles bg

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './App.scss';
 import Home from './components/Home/Home'
 import Covid from './components/Covid/Covid'
-
+import ParticlesBackground from './components/ParticlesBackground/ParticlesBackground'
 
 function App() {
   
   return (
     <div>
-
+      <ParticlesBackground />
       <Home/>
       <Covid />
     </div>
